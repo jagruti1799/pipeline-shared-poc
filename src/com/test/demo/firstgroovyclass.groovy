74a6fa51d99aa623dc8a31c1b213d2c3e7f50cf2
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-08-31 14:52:07 +0530

    Update firstgroovyclass.groovy

--- src/com/test/demo/firstgroovyclass.groovy
+++ src/com/test/demo/firstgroovyclass.groovy
@@ -1,10 +1,5 @@
 package com.test.demo
 
-def checkout([
-        $class: 'GitSCM', 
-        branches: [[name: '*/main']], 
-        doGenerateSubmoduleConfigurations: false, 
-        extensions: [[$class: 'CleanCheckout']], 
-        submoduleCfg: [], 
-        userRemoteConfigs: [[credentialsId: 'None', url: 'https://github.com/jagruti1799/Jenkins-Terraform.git']]
-    ])
+checkout([$class: 'GitSCM',
+         branches: [[name: '*/master']],
+         userRemoteConfigs: [[url: 'https://github.com/jagruti1799/Jenkins-Terraform.git']]]) 
