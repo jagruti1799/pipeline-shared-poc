bba61ca1e3817aa20e6e2fabef6797a99325bbb3
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-09-01 15:01:25 +0530

    Update firstgroovyclass.groovy

--- src/com/test/demo/firstgroovyclass.groovy
+++ src/com/test/demo/firstgroovyclass.groovy
@@ -3,20 +3,10 @@ package com.test.demo
 
 def checkoutGitRepository(repo){
     dir(path) {
-        checkout(
-            changelog:true,
-            poll: poll,
-            scm: [
-                $class: 'GitSCM',
-                branches: [[name: "*/${branch}"]],
-            doGenerateSubmoduleConfigurations: false,
-            extensions: [
-                [$class: 'CheckoutOption', timeout: timeout],
-                [$class: 'CloneOption', depth: depth, noTags: false, reference: '', shallow: depth > 0, timeout: timeout]],
-            submoduleCfg: [],
-            userRemoteConfigs: [[url: 'https://github.com/jagruti1799/Jenkins-Terraform.git']]]
-        )
-        sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+        checkout([$class: 'GitSCM',
+		 branches: [[name: '*/main']],
+		 userRemoteConfigs: [[url: 'https://github.com/jagruti1799/Jenkins-Terraform.git']]])
+         sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
     }
 }
 return this
