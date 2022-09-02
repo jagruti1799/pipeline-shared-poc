2cff8c9f0e98ce2df460f35c8925112c6a53a8e3
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-09-02 18:18:00 +0530

    Update firstgroovyclass.groovy

--- src/com/test/demo/firstgroovyclass.groovy
+++ src/com/test/demo/firstgroovyclass.groovy
@@ -3,11 +3,10 @@ package com.test.demo;
 
 def checkoutGitRepository(repo){
 	
-	 git url: "https://github.com/jagruti1799/Jenkins-Terraform.git"
-   /* dir(path) {
+   dir(path) {
         checkout([$class: 'GitSCM',
 		 branches: [[name: '*/main']],
 		 userRemoteConfigs: [[url: 'https://github.com/jagruti1799/Jenkins-Terraform.git']]])
-    } */
+    } 
 }
 return this
