6791e1d4db720f741b62fdd3d2d28eef31d626e0
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-09-05 17:22:21 +0530

    Update terraform.groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,15 +1,61 @@
+def AGENT_LABEL = 'Slave1'
 def terraformInit () {
-    sh ('terraform init')
+    sh 'terraform init'
 }
 
 def terraformPlan () {
-    sh ('terraform plan')
+    sh 'terraform plan'
 }
 
 def terraformApply () {
-    sh ('terraform apply -auto-approve')
+    sh 'terraform apply -auto-approve'
 }
 
 def terraformDestroy () {
-    sh ('terraform destroy -auto-approve')
+    sh 'terraform destroy -auto-approve'
 }
+
+pipeline {
+ agent {
+  label 'Slave'
+ }
+ tools { terraform 'terraform' }
+ options { timestamps () }
+ stages {
+       stage('checkout'){
+           steps {
+                  gitCheckout(
+                      branch: "main",
+                      url: "https://github.com/jagruti1799/Jenkins-Terraform.git")
+                }
+       }
+        stage ('terraform init') {
+            steps {
+                terraformInit ()
+            }
+        }
+        stage ('terraform plan') {
+            steps {
+                terraformInit ()
+            }
+        }
+        stage ('terraform apply') {
+            steps {
+                terraformApply ()
+            }
+        }
+        stage ('Print hostname of created slave') {
+           agent {
+                label "${AGENT_LABEL}"
+            }
+            options {
+            timeout(time: 3, unit: 'SECONDS')
+            }
+                steps {
+                    sh '''hostname
+                        hostname -I'''
+            }
+        }
+    }
+}
+
