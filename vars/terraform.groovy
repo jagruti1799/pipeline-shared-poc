3355d4490b62dd4a880af34d7e3b91e9d1cd9e82
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-09-05 17:02:51 +0530

    Update terraform.groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,13 +1,15 @@
-import devops.common.utils
+def terraformInit () {
+    sh ('terraform init')
+}
 
-def apply(config) {
-  // set terraform env for automation
-  env.TF_IN_AUTOMATION = true
+def terraformPlan () {
+    sh ('terraform plan')
+}
 
-  // input checking
-  assert config.configPath : '/usr/bin'
-  assert fileExists(config.configPath) : "Terraform config/plan ${config.configPath} does not exist!"
-  config.bin = config.bin ?: 'terraform'
+def terraformApply () {
+    sh ('terraform apply -auto-approve')
+}
 
-  String cmd = "${config.bin} apply -input=false -no-color -auto-approve"
-  }
+def terraformDestroy () {
+    sh ('terraform destroy -auto-approve')
+}
