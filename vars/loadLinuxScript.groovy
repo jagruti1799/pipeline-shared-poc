7ab30eda0bcb9449a63cbbb1a55dc3705f1b2e06
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-09-07 12:49:00 +0530

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,4 +1,4 @@
-def call(name) {
+def call(Map config = [:]) { 
   def scriptcontents = libraryResource "com/demo/script/linux/${config.name}"
   writeFile file: "${config.name}", text: scriptcontents
   sh "chmod a+x ./${config.name}"
