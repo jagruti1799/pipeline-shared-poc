34eb85614b8ef70c68df7100853609926e8defd3
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-09-07 13:12:53 +0530

    Delete loadPythonScript.sh

--- vars/loadPythonScript.sh
+++
@@ -1,5 +0,0 @@
-def callname(Map config = [:]) { 
-  def scriptcontentspy = libraryResource "com/planetpope/scripts/linux/${config.name1}"    
-  writeFile file: "${config.name1}", text: scriptcontentspy 
-  sh "chmod a+x ./${config.name1}"
-} 
