788adf4aa72cb6e7d0a124827a5bb681c6023376
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-09-07 17:46:03 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,7 @@
 def call(Map stageParams) {
 
-    checkout([$class: 'GitSCM',
-        branches: [[name: stageParams.name]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
+checkout([$class: 'GitSCM',
+		 branches: [[name: '*/main']],
+		 userRemoteConfigs: [[url: 'https://github.com/jagruti1799/Jenkins-Terraform.git']]])
     ])
  }
