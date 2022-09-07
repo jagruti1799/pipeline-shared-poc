1f2e9d7a9ab5f96a493da9687be863c59bc73706
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-09-07 13:11:26 +0530

    Update loadPythonScript.sh

--- vars/loadPythonScript.sh
+++ vars/loadPythonScript.sh
@@ -1,5 +1,5 @@
-def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "com/planetpope/scripts/linux/${config.name1}"    
-  writeFile file: "${config.name1}", text: scriptcontents 
+def callname(Map config = [:]) { 
+  def scriptcontentspy = libraryResource "com/planetpope/scripts/linux/${config.name1}"    
+  writeFile file: "${config.name1}", text: scriptcontentspy 
   sh "chmod a+x ./${config.name1}"
 } 
