b3c8bebf25032243766e88697125bebe6c831d94
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-09-07 11:58:01 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,5 @@
 def call() {
-  def scriptcontents = libraryResource "com/planetpope/scripts/linux/helloworld.py"
+  def scriptcontents = libraryResource "com/demo/script/linux/helloworld.py"
   sh "echo hello world"
   sh "python3 helloworld.py"
 }
