c5d5f97fff6f3f7b4db4cb7222fda6dee561e564
jagruti1799 <110963747+jagruti1799@users.noreply.github.com>
2022-09-07 13:12:33 +0530

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,6 +1,4 @@
 def call(Map config = [:]) {
   loadLinuxScript(name: 'hello-world.sh')
-  loadPythonScript(name1: 'helloworld.py')
   sh "./hello-world.sh ${config.name}"
-  sh "python3 helloworld.py"
 }
